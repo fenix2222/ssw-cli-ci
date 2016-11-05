3687537dec1af8526e1fd034d69f1a74b1469962
Igor Goldobin <igor.goldobin@gmail.com>
2016-11-05 12:01:01 +1000

    Updated deployment script to install angular cli

--- deploy.cmd
+++ deploy.cmd
@@ -90,7 +90,7 @@ echo Handling node.js deployment.
 
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%\approot" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
@@ -107,8 +107,8 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 
 :: 4. NG Build
 echo Running 'ng build' command in %DEPLOYMENT_TARGET%
-call :ExecuteCmd !NPM_CMD! install -g angular-cli
 call :ExecuteCmd node %DEPLOYMENT_TARGET%\node_modules\angular-cli\bin\ng build --target=production
+xcopy %DEPLOYMENT_TARGET%\approot\dist\* %DEPLOYMENT_TARGET%\wwwroot /e /i /h
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
